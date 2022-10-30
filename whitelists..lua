5c2a894c90afc3061f9ae4b55ddc362375297a5e
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-10-30 18:23:49 -0400

    Update whitelists..lua

--- whitelists..lua
+++ whitelists..lua
@@ -1,16 +1,15 @@
 return {
     ["OWNER"] = {
 
-        ["users"] = {"6374f7797444e8fec5dced39e51e6e1fd1c26e40885fd03f740701f8f704fb27f187be2a1890fa502aabc178284208ba7aea2765d1722519ac2ddcb14624b0d0"},
-        tagtext = "KERSHY ADMIN OWNER",
-        tagcolor = Color3.fromRGB(230, 0, 230)
+        ["users"] = {"6374f7797444e8fec5dced39e51e6e1fd1c26e40885fd03f740701f8f704fb27f187be2a1890fa502aabc178284208ba7aea2765d1722519ac2ddcb14624b0d0"}
+        
+       
 
     },
     ["PRIVATE"] = {
 
-        ["users"] = {},
-        tagtext = "KERSHY ADMIN PRIVATE",
-        tagcolor = color3.fromRGB(300, 0, 300)
+        ["users"] = {}
+       
 
     }
 
