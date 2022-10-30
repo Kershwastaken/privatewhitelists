d98e87f7c5a5dae6821f8153e58542a56df296d6
Thanosdagamer <106916828+Thanosdagamer@users.noreply.github.com>
2022-10-30 18:36:24 -0400

    Update whitelists..lua

--- whitelists..lua
+++ whitelists..lua
@@ -1,16 +1,2 @@
 return {
-    ["OWNER"] = {
-
-        ["users"] = {"6374f7797444e8fec5dced39e51e6e1fd1c26e40885fd03f740701f8f704fb27f187be2a1890fa502aabc178284208ba7aea2765d1722519ac2ddcb14624b0d0"}
-        
-       
-
-    },
-    ["PRIVATE"] = {
-
-        ["users"] = {}
-       
-
-    }
-
-}
+    "6374f7797444e8fec5dced39e51e6e1fd1c26e40885fd03f740701f8f704fb27f187be2a1890fa502aabc178284208ba7aea2765d1722519ac2ddcb14624b0d0"}
